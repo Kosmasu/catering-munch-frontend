5507231184a29aa4468c1ebb869cbdad9b75b06c
Ignatius Odi <ignodi@gmail.com>
2022-11-30 00:18:11 +0700

    init customer

--- src/views/Customer/Home.vue
+++ src/views/Customer/Home.vue
@@ -1,7 +1,20 @@
 <template>
-  <div></div>
+  <navbar />
+  <div>
+    Pesanan Anda
+    <div>Foto Detail Item</div>
+  </div>
+  <div>Detail Pesanan</div>
 </template>
 <script>
-export default {};
+import Navbar from "@/components/CustomerNavbar.vue";
+export default {
+  name: "CustomerHome",
+  components: {
+    Navbar,
+  },
+  data() {},
+  methods: {},
+};
 </script>
 <style></style>
