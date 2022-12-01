b49e01a1e0bb9d490964a7b5ee3cd8e7e37c078d
Kenny <ken_link26@yahoo.com>
2022-12-01 19:21:26 +0700

    added font-awesome + axios

--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSpinner)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
