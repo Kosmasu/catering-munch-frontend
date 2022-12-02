baaffdf2aeec86a722fc1a60b0055aa597ec3f87
Ignatius Odi <ignodi@gmail.com>
2022-12-03 02:27:33 +0700

    admin request

--- src/MunchService.js
+++ src/MunchService.js
@@ -3,7 +3,9 @@ import axios from "axios";
 class MunchService {
   constructor() {
     if (this instanceof StaticClass) {
-      throw Error('MunchService is a static class, thus cannot be instantiated.');
+      throw Error(
+        "MunchService is a static class, thus cannot be instantiated."
+      );
     }
   }
 
@@ -11,16 +13,28 @@ class MunchService {
     // baseURL ini sesuaikan dengan url backend Anda
     baseURL: "http://127.0.0.1:8000/api",
     headers: {
-      "Content-type": "application/json"
-    }
+      "Content-type": "application/json",
+    },
   });
 
   static getAllCustomers() {
-    return this.http.get("/admin/users/getAllCustomers")
+    return this.http.get("/admin/users/getAllCustomers");
   }
 
   static getAllProviders() {
-    return this.http.get("/admin/users/getAllProviders")
+    return this.http.get("/admin/users/getAllProviders");
+  }
+
+  static banUser(id) {
+    return this.http.get("/admin/users/banUser/" + id);
+  }
+
+  static unbanUser(id) {
+    return this.http.get("/admin/users/unbanUser/" + id);
+  }
+
+  static approveProvider(id) {
+    return this.http.get("/admin/users/approveProvider/" + id);
   }
 }
 
--- src/stores/AdminStore.js
+++ src/stores/AdminStore.js
@@ -9,6 +9,7 @@ export const useAdminStore = defineStore("AdminStore", {
   state: () => ({
     customers: undefined,
     providers: undefined,
+    result: undefined,
   }),
   // getters itu computed properties (tidak boleh ada parameter)
   getters: {},
@@ -24,13 +25,82 @@ export const useAdminStore = defineStore("AdminStore", {
           console.error(error);
         });
     },
-    customerSearch(query) {
-      if (this.customers) {
-        return this.customers.filter((customer) => {
-          return (
-            customer.users_nama.toLowerCase().indexOf(query.toLowerCase()) >
-              -1 && customer.users_status != "menunggu"
-          );
+    async fetchProviders() {
+      await MunchService.getAllProviders()
+        .then((response) => {
+          this.providers = response.data;
+          console.log("this.providers:", this.providers);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    async banUser(role, id) {
+      await MunchService.banUser(id)
+        .then((response) => {
+          this.result = response.data;
+          console.log("user id", id, "banned");
+          if (role == "customer") {
+            this.fetchCustomers();
+          } else {
+            this.fetchProviders();
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    async unbanUser(role, id) {
+      await MunchService.unbanUser(id)
+        .then((response) => {
+          this.result = response.data;
+          console.log("user id", id, "unbanned");
+          if (role == "customer") {
+            this.fetchCustomers();
+          } else {
+            this.fetchProviders();
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    async approveProvider(id) {
+      await MunchService.approveProvider(id)
+        .then((response) => {
+          this.result = response.data;
+          console.log("user id", id, "approved");
+          this.fetchProviders();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    usersSearch(role, query) {
+      if (role == "provider") {
+        if (this.providers) {
+          return this.providers.filter((provider) => {
+            return (
+              provider.users_nama.toLowerCase().indexOf(query.toLowerCase()) >
+                -1 && provider.users_status != "menunggu"
+            );
+          });
+        }
+      } else {
+        if (this.customers) {
+          return this.customers.filter((customer) => {
+            return (
+              customer.users_nama.toLowerCase().indexOf(query.toLowerCase()) >
+              -1
+            );
+          });
+        }
+      }
+    },
+    providerWaiting() {
+      if (this.providers) {
+        return this.providers.filter((provider) => {
+          return provider.users_status == "menunggu";
         });
       }
     },
