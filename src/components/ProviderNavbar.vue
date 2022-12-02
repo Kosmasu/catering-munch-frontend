10acd975d0109e76dc3a5f3c857f5892a9439552
Kenny <ken_link26@yahoo.com>
2022-12-02 10:52:21 +0700

    Update ProviderNavbar.vue
    added container,
    
    container untuk responsive

--- src/components/ProviderNavbar.vue
+++ src/components/ProviderNavbar.vue
@@ -1,66 +1,68 @@
 <template>
-  <div class="navbar bg-accent">
-    <div class="navbar-start">
-      <div class="dropdown">
-        <label tabindex="0" class="btn btn-ghost lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+  <div class="bg-accent">
+    <div class="navbar container mx-auto">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16"
+            <li v-bind:class="getPage('provider-menus')">
+              <RouterLink to="/provider/menus">Menus</RouterLink>
+            </li>
+            <li v-bind:class="getPage('provider-history')">
+              <RouterLink to="/provider/history">History</RouterLink>
+            </li>
+          </ul>
+        </div>
+        <button
+          class="btn btn-ghost normal-case text-xl text-accent-content hover:btn-accent-focus"
+        >
+          <RouterLink to="/provider">Munch</RouterLink>
+        </button>
+        <div class="hidden lg:flex">
+          <ul class="menu menu-horizontal p-0 space-x-2 rounded-full">
+            <li v-bind:class="getPage('provider-menus')">
+              <RouterLink to="/provider/menus">Menus</RouterLink>
+            </li>
+            <li v-bind:class="getPage('provider-history')">
+              <RouterLink to="/provider/history">History</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="navbar-end">
+        <div class="dropdown dropdown-hover">
+          <label tabindex="0" class="btn">
+              Welcome, Provider
+            <img
+              src="https://placeimg.com/80/80/people"
+              class="ml-4 -mr-4 w-11 rounded-full"
             />
-          </svg>
-        </label>
-        <ul
-          tabindex="0"
-          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li v-bind:class="getPage('provider-menus')">
-            <RouterLink to="/provider/menus">Menus</RouterLink>
-          </li>
-          <li v-bind:class="getPage('provider-history')">
-            <RouterLink to="/provider/history">History</RouterLink>
-          </li>
-        </ul>
-      </div>
-      <button
-        class="btn btn-ghost normal-case text-xl text-accent-content hover:btn-accent-focus"
-      >
-        <RouterLink to="/provider">Munch</RouterLink>
-      </button>
-      <div class="hidden lg:flex">
-        <ul class="menu menu-horizontal p-0 space-x-2 rounded-full">
-          <li v-bind:class="getPage('provider-menus')">
-            <RouterLink to="/provider/menus">Menus</RouterLink>
-          </li>
-          <li v-bind:class="getPage('provider-history')">
-            <RouterLink to="/provider/history">History</RouterLink>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="navbar-end">
-      <div class="dropdown dropdown-hover">
-        <label tabindex="0" class="btn">
-            Welcome, Provider
-          <img
-            src="https://placeimg.com/80/80/people"
-            class="ml-4 -mr-4 w-11 rounded-full"
-          />
-        </label>
-        <ul
-          tabindex="0"
-          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li><RouterLink to="/">Logout</RouterLink></li>
-        </ul>
+          </label>
+          <ul
+            tabindex="0"
+            class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li><RouterLink to="/">Logout</RouterLink></li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
