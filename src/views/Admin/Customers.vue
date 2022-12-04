79ffd3b9bbef5694fedde4562aaa992cabd17375
Kenny <ken_link26@yahoo.com>
2022-12-04 16:19:46 +0700

    Update Customers.vue

--- src/views/Admin/Customers.vue
+++ src/views/Admin/Customers.vue
@@ -22,62 +22,62 @@
             class="input input-primary input-bordered"
           />
         </div>
-        <div class="">
-          <table class="table table-compact text-center w-full">
-            <thead>
-              <tr>
-                <th class="bg-primary text-primary-content">ID</th>
-                <th class="bg-primary text-primary-content">Nama</th>
-                <th class="bg-primary text-primary-content">Email</th>
-                <th class="bg-primary text-primary-content">Alamat</th>
-                <th class="bg-primary text-primary-content">Nomor Telepon</th>
-                <th class="bg-primary text-primary-content">Status</th>
-                <th class="bg-primary text-primary-content">Action</th>
-              </tr>
-            </thead>
-            <tbody v-if="customers">
-              <tr
-                v-for="customer in usersSearch('customer', query)"
-                class="hover"
-              >
-                <td>{{ customer.users_id }}</td>
-                <td>{{ customer.users_nama }}</td>
-                <td>{{ customer.users_email }}</td>
-                <td>{{ customer.users_alamat }}</td>
-                <td>{{ customer.users_telepon }}</td>
-                <td>{{ customer.users_status.toUpperCase() }}</td>
-                <td>
-                  <button
-                    v-if="customer.users_status == 'aktif'"
-                    @click="ban(customer.users_id)"
-                    class="btn btn-primary rounded-lg"
-                  >
-                    Ban
-                  </button>
-                  <button
-                    v-else
-                    @click="unban(customer.users_id)"
-                    class="btn btn-error rounded-lg"
-                  >
-                    Unban
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-            <tbody v-else>
-              <tr>
-                <td class="text-center" colspan="7">
-                  <font-awesome-icon
-                    icon="fa-solid fa-spinner"
-                    class="text-6xl animate-spin"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <div class="w-full flex justify-center">
-            <pagination-vue :maxPage="10" v-model:currentPage="currentPage"/>
-          </div>
+      </div>
+      <div class="">
+        <table class="table table-compact text-center w-full">
+          <thead>
+            <tr>
+              <th class="bg-primary text-primary-content">ID</th>
+              <th class="bg-primary text-primary-content">Nama</th>
+              <th class="bg-primary text-primary-content">Email</th>
+              <th class="bg-primary text-primary-content">Alamat</th>
+              <th class="bg-primary text-primary-content">Nomor Telepon</th>
+              <th class="bg-primary text-primary-content">Status</th>
+              <th class="bg-primary text-primary-content">Action</th>
+            </tr>
+          </thead>
+          <tbody v-if="customers">
+            <tr
+              v-for="customer in usersSearch('customer', query)"
+              class="hover"
+            >
+              <td>{{ customer.users_id }}</td>
+              <td>{{ customer.users_nama }}</td>
+              <td>{{ customer.users_email }}</td>
+              <td>{{ customer.users_alamat }}</td>
+              <td>{{ customer.users_telepon }}</td>
+              <td>{{ customer.users_status.toUpperCase() }}</td>
+              <td>
+                <button
+                  v-if="customer.users_status == 'aktif'"
+                  @click="ban(customer.users_id)"
+                  class="btn btn-primary rounded-lg"
+                >
+                  Ban
+                </button>
+                <button
+                  v-else
+                  @click="unban(customer.users_id)"
+                  class="btn btn-error rounded-lg"
+                >
+                  Unban
+                </button>
+              </td>
+            </tr>
+          </tbody>
+          <tbody v-else>
+            <tr>
+              <td class="text-center" colspan="7">
+                <font-awesome-icon
+                  icon="fa-solid fa-spinner"
+                  class="text-6xl animate-spin"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="w-full flex justify-center">
+          <pagination-vue :maxPage="10" v-model:currentPage="currentPage"/>
         </div>
       </div>
     </div>
