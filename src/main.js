b0268e78cb0e83de8381bb7398c9d610be497e7e
Ignatius Odi <ignodi@gmail.com>
2022-12-09 18:46:59 +0700

    add vue calendar

--- src/main.js
+++ src/main.js
@@ -1,52 +1,64 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import axios from 'axios'
-import MunchService from './MunchService'
-import { useAuthStore } from './stores/Auth/AuthStore'
+import "./assets/main.css";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import MunchService from "./MunchService";
+import { useAuthStore } from "./stores/Auth/AuthStore";
 
-library.add(faSpinner)
+/* https://vcalendar.io/vue-3.html */
+import "v-calendar/dist/style.css";
+import VCalendar from "v-calendar";
 
-const app = createApp(App)
+library.add(faSpinner);
 
-axios.defaults.withCredentials = true
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.component('font-awesome-icon', FontAwesomeIcon)
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.use(VCalendar, {});
+
+app.mount("#app");
 
 // https://axios-http.com/docs/interceptors
-MunchService.http.interceptors.request.use(async response => {
-  // console.log('response request interceptor:',response);
-  if (response.method != "get") {
-    const authStore = useAuthStore()
-    await authStore.sanctum()
+MunchService.http.interceptors.request.use(
+  async (response) => {
+    // console.log('response request interceptor:',response);
+    if (response.method != "get") {
+      const authStore = useAuthStore();
+      await authStore.sanctum();
+    }
+    return response;
+  },
+  (error) => {
+    // console.log('error request interceptor:',error);
+    return Promise.reject(error);
   }
-  return response;
-}, error => {
-  // console.log('error request interceptor:',error); 
-  return Promise.reject(error);
-});
+);
 
-MunchService.http.interceptors.response.use(async response => {
-  // console.log('response response interceptor:',response);
-  return response;
-}, error => {
-  // console.log('error response interceptor:',error);
-  if (error.response.status == 401 || error.response.status == 419) {
-    const authStore = useAuthStore()
-    authStore.removeUserFromLocalStorage()
-    router.push({ name: 'login' })
+MunchService.http.interceptors.response.use(
+  async (response) => {
+    // console.log('response response interceptor:',response);
+    return response;
+  },
+  (error) => {
+    // console.log('error response interceptor:',error);
+    if (error.response.status == 401 || error.response.status == 419) {
+      const authStore = useAuthStore();
+      authStore.removeUserFromLocalStorage();
+      router.push({ name: "login" });
+    }
+    return Promise.reject(error);
   }
-  return Promise.reject(error);
-});
+);
