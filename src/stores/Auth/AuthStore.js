5bd0feae3fbc7801bd876b5a2276011bd3a3c33e
Ignatius Odi <ignodi@gmail.com>
2022-12-09 18:27:17 +0700

    add id

--- src/stores/Auth/AuthStore.js
+++ src/stores/Auth/AuthStore.js
@@ -5,59 +5,62 @@ import router from "@/router";
 export const useAuthStore = defineStore("AuthStore", {
   state: () => ({
     user: {
+      id: null,
       nama: "guest",
       role: "guest",
       saldo: null,
-    }
+    },
   }),
   getters: {
     firstName() {
-      return this.user.nama.split(" ")[0]
-    }
+      return this.user.nama.split(" ")[0];
+    },
   },
   actions: {
     async sanctum() {
-      await MunchService.sanctum()
+      await MunchService.sanctum();
     },
     async me() {
       await MunchService.me()
-        .then(response => {
+        .then((response) => {
           this.setUser(
+            response.data.data.users_id,
             response.data.data.users_nama,
             response.data.data.users_role,
-            response.data.data.users_saldo,
-          )
-          localStorage.setItem("user", JSON.stringify(this.user))
+            response.data.data.users_saldo
+          );
+          localStorage.setItem("user", JSON.stringify(this.user));
         })
-        .catch(error => {
-          console.log('error me:', error);
-        })
+        .catch((error) => {
+          console.log("error me:", error);
+        });
     },
     async logout() {
       await MunchService.logout().then((response) => {
         if (response.data.status == "success") {
-          this.removeUserFromLocalStorage()
-          router.push({ name: "landing-page" })
+          this.removeUserFromLocalStorage();
+          router.push({ name: "landing-page" });
         } else {
-          console.log(response.data.status)
+          console.log(response.data.status);
         }
       });
     },
-    setUser(nama, role, saldo) {
-      this.user.nama = nama
-      this.user.role = role
-      this.user.saldo = saldo
+    setUser(id, nama, role, saldo) {
+      this.user.id = id;
+      this.user.nama = nama;
+      this.user.role = role;
+      this.user.saldo = saldo;
     },
     getUserFromLocalStorage() {
-      this.user = JSON.parse(localStorage.getItem("user"))
+      this.user = JSON.parse(localStorage.getItem("user"));
       if (!this.user) {
-        this.$reset()
+        this.$reset();
       }
-      return this.user
+      return this.user;
     },
     removeUserFromLocalStorage() {
-      this.$reset()
-      localStorage.removeItem("user")
-    }
+      this.$reset();
+      localStorage.removeItem("user");
+    },
   },
 });
