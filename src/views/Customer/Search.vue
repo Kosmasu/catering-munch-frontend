b11f3f4ec8af2701a16084759f675b94b40c9ba2
Ignatius Odi <ignodi@gmail.com>
2022-12-18 02:09:28 +0700

    update customer-search (not done)

--- src/views/Customer/Search.vue
+++ src/views/Customer/Search.vue
@@ -1,41 +1,109 @@
 <template>
   <div class="container">
-    <div class="text-center m-8">
+    <div class="flex space-x-4 justify-center m-8">
       <input
         type="text"
         v-model="query"
         placeholder="Search…"
-        class="input input-primary input-bordered"
+        class="input input-primary"
       />
+      <select v-model="filter" @change="onFilterChange" class="select select-primary">
+        <option value="menu_nama">Nama</option>
+        <option value="menu_harga">Harga</option>
+      </select>
+      <select v-model="sort" @change="onSortChange" class="select select-primary">
+        <option value="asc">Ascending</option>
+        <option value="desc">Descending</option>
+      </select>
     </div>
-    <div class="overflow-x-auto">
-      <table class="table table-compact text-center w-full">
-        <thead>
-          <tr>
-            <th colspan="5" class="bg-primary">Hasil pencarian</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>
-              <img src="@/assets/munch.png" class="w-24" />
-            </td>
-            <td>Detail Item</td>
-            <td>Harga Item</td>
-            <button
-              @click="detail(menu.menu_id)"
-              class="btn btn-primary rounded-lg"
-            >
+    <table class="table table-compact text-center w-full">
+      <thead>
+        <tr>
+          <th colspan="4" class="bg-primary">Hasil pencarian</th>
+        </tr>
+      </thead>
+      <tbody v-if="menus">
+        <tr v-for="menu in menus.data">
+          <td>
+            <img
+              :src="'http://localhost:8000/storage/' + menu.menu_foto"
+              class="w-24"
+            />
+          </td>
+          <td class="capitalize">
+            <span class="font-bold">{{ menu.menu_nama }}</span>
+            - {{ menu.users.users_nama }}
+          </td>
+          <td>Rp, {{ menu.menu_harga.toLocaleString("id-ID") }},00</td>
+          <td>
+            <button @click="detail(menu.menu_id)" class="btn btn-primary">
               Detail
             </button>
-          </tr>
-        </tbody>
-      </table>
+          </td>
+        </tr>
+      </tbody>
+      <tbody v-else>
+        <tr>
+          <td class="text-center" colspan="4">
+            <font-awesome-icon
+              icon="fa-solid fa-spinner"
+              class="text-6xl animate-spin"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div v-if="this.menus" class="p-2">
+      <pagination-vue v-model="currentPage" :paginatedData="this.menus" />
     </div>
   </div>
 </template>
 <script>
-export default {};
+import { mapActions, mapState } from "pinia";
+import { useCustomerStore } from "@/stores/CustomerStore";
+import PaginationVue from "@/components/Pagination.vue";
+import router from "@/router";
+
+export default {
+  name: "CustomerSearch",
+  components: {
+    PaginationVue,
+  },
+  data() {
+    return {
+      query: "",
+      filter: "menu_nama",
+      sort: "asc",
+      currentPage: 1,
+    };
+  },
+  methods: {
+    ...mapActions(useCustomerStore, ["fetchMenus"]),
+    detail(id) {
+      router.push({ name: "customer-search-provider", params: { id } });
+    },
+    onFilterChange() {
+      
+    },
+    onSortChange() {
+
+    },
+  },
+  computed: {
+    ...mapState(useCustomerStore, ["menus"]),
+  },
+  created() {
+    this.fetchMenus();
+  },
+  watch: {
+    currentPage(newCurrentPage, oldCurrentPage) {
+      this.fetchMenus(newCurrentPage);
+    },
+    query(newQuery, oldQuery) {
+      this.currentPage = 1;
+      this.fetchMenus(this.currentPage, newQuery);
+    },
+  },
+};
 </script>
 <style></style>
