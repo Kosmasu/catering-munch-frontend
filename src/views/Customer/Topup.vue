3f83216fe66330edd40c1533b30cf0296c5c502e
Ignatius Odi <ignodi@gmail.com>
2022-12-18 00:37:42 +0700

    update customer topup

--- src/views/Customer/Topup.vue
+++ src/views/Customer/Topup.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="container">
-    <div class="m-4 font-semibold text-2xl text-center">Top Up</div>
+    <div class="m-4 font-semibold text-3xl text-center">Top Up</div>
     <div class="rounded-lg border-2 p-4">
-      <form @submit.prevent="submit" class="form-control">
+      <form @submit.prevent="this.topup" class="form-control">
         <!-- NOMINAL -->
         <input-vue
-          v-model="topup.nominal"
+          v-model="formTopup.nominal"
           name="nominal"
           type="number"
-          label="Nominal Top Up"
+          label="Nominal"
           :errors="this.errorData.errors"
         />
         <!-- PASSWORD -->
         <input-vue
-          v-model="topup.password"
+          v-model="formTopup.password"
           name="password"
           type="password"
           label="Password"
@@ -24,41 +24,27 @@
             {{ this.errorData.message }}
           </span>
         </label>
-        <button class="btn btn-primary w-1/2 mx-auto">Topup</button>
+        <button class="btn btn-primary w-1/2 mx-auto">Top Up</button>
       </form>
     </div>
   </div>
 </template>
 <script>
-import { mapActions, mapWritableState } from "pinia";
+import { mapState, mapActions, mapWritableState } from "pinia";
 import { useCustomerStore } from "@/stores/CustomerStore.js";
 import InputVue from "@/components/Inputs/Input.vue";
 
 export default {
   name: "CustomerTopup",
-  data() {
-    return {
-      nominal: "",
-      password: "",
-    };
-  },
   components: {
     InputVue,
   },
-  computed: {
-    ...mapWritableState(useCustomerStore, ["form", "errorData"]),
-  },
   methods: {
     ...mapActions(useCustomerStore, ["topup"]),
-    async submit() {
-      await this.topup().then((response) => {
-        console.log("response:", response);
-        if (response instanceof Error) {
-          this.errorData = response.response.data;
-          console.log("this.errorData:", this.errorData);
-        }
-      });
-    },
+  },
+  computed: {
+    ...mapWritableState(useCustomerStore, ["formTopup"]),
+    ...mapState(useCustomerStore, ["errorData"]),
   },
 };
 </script>
